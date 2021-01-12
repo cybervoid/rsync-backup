e59cae883804aba5a634429d4ed684e63993a17c
Rafael Gil <rafaelgil83@gmail.com>
2021-01-11 22:28:18 -0500

    added rsync

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
 echo "Running rsync"
-#rsync --progress -huvarz /mnt/ "$DEST"
-#ls -la /home/user/.ssh
-pwd
-
-ls -la /home/user
-ls -la /home/user/.ssh
-
+rsync --progress -huvarz /mnt/ "$DEST"
 
